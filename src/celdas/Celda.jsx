b8121572e2acb9d00b4de333a5291846ec4af028
Mario Cantelar <mcantelar@gmail.com>
2023-04-10 03:38:44 +0200

    cursor movido a celda

--- src/celdas/Celda.jsx
+++ src/celdas/Celda.jsx
@@ -6,7 +6,11 @@ const Celda = ({ fila, columna, ficha }) => {
     // let letra = String.fromCharCode(columna + "A".charCodeAt(0));
 
     return (
-        <div className={estilos.celda}>
+        <div
+            className={
+                estilos.celda + " " + (ficha ? estilos.celdaConFicha : "")
+            }
+        >
             {/* {fila}-{letra} */}
             {ficha ? ficha : undefined}
         </div>
