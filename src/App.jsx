2ccceaed03a69839a2845e22f6c6348dfd15377b
Mario Cantelar <mcantelar@gmail.com>
2023-04-10 23:02:59 +0200

    cache buster

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+
 import Tablero from "./tablero/Tablero";
+import CacheBuster from "./scripts/cacheBuster";
 
 import "./App.css";
 
 const App = (props) => {
     return (
-        <div>
-            <Tablero />
-        </div>
+        <CacheBuster>
+            {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+                if (loading) return null;
+                if (!loading && !isLatestVersion) {
+                    refreshCacheAndReload();
+                }
+
+                return <Tablero />;
+            }}
+        </CacheBuster>
     );
 };
 
---
+++ src/scripts/cacheBuster.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import packageJson from "../../package.json";
+
+// version from response - first param, local version second param
+const semverGreaterThan = (versionA, versionB) => {
+    const versionsA = versionA.split(/\./g);
+
+    const versionsB = versionB.split(/\./g);
+    while (versionsA.length || versionsB.length) {
+        const a = Number(versionsA.shift());
+
+        const b = Number(versionsB.shift());
+        // eslint-disable-next-line no-continue
+        if (a === b) continue;
+        // eslint-disable-next-line no-restricted-globals
+        return a > b || isNaN(b);
+    }
+    return false;
+};
+
+class CacheBuster extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            isLatestVersion: false,
+            refreshCacheAndReload: (reload = true) => {
+                console.log("Limpiando cache y recargando aplicación...");
+
+                try {
+                    if (caches) {
+                        // Service worker cache should be cleared with caches.delete()
+                        caches.keys().then(function(names) {
+                            for (let name of names) caches.delete(name);
+                        });
+                    }
+                } catch (error) {}
+
+                if (reload) {
+                    // delete browser cache and hard reload
+                    window.location.reload(true);
+                }
+            },
+        };
+    }
+
+    componentDidMount() {
+        if (process.env.NODE_ENV !== "production") {
+            // Solo comprobamos la versión en producción.
+            this.setState({ loading: false, isLatestVersion: true });
+            return;
+        }
+
+        // Traemos siempre el meta.json sin cachear para tener la última versión.
+        fetch("/meta.json?f=" + new Date().getTime())
+            .then((response) => response.json())
+            .then((meta) => {
+                const latestVersion = meta.version;
+                const currentVersion = packageJson.version;
+
+                const shouldForceRefresh = semverGreaterThan(
+                    latestVersion,
+                    currentVersion
+                );
+                if (shouldForceRefresh) {
+                    console.log(
+                        `Tenemos una nueva versión - ${latestVersion}. Debemos forzar el refresco de la aplicación.`
+                    );
+                    this.setState({ loading: false, isLatestVersion: false });
+                } else {
+                    console.log(
+                        `Tienes la última versión disponible - ${latestVersion}. No se necesita actualizar la caché.`
+                    );
+                    this.setState({ loading: false, isLatestVersion: true });
+                }
+            })
+            .catch((error) => {
+                console.log(
+                    `Versión anterior desconocida. Forzamos el refresco de la aplicación.`
+                );
+                this.state.refreshCacheAndReload(false);
+                this.setState({ loading: false, isLatestVersion: true });
+            });
+    }
+    render() {
+        const { loading, isLatestVersion, refreshCacheAndReload } = this.state;
+        return this.props.children({
+            loading,
+            isLatestVersion,
+            refreshCacheAndReload,
+        });
+    }
+}
+
+export default CacheBuster;
