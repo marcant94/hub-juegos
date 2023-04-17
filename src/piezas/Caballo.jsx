d085db9db50b15e7e0cad10295f676bc7c5bf02d
Mario Cantelar <mcantelar@gmail.com>
2023-04-17 19:28:39 +0200

    añadido movimientos del caballo

--- src/piezas/Caballo.jsx
+++ src/piezas/Caballo.jsx
@@ -5,7 +5,26 @@ const Caballo = ({ fila, columna, color }) => {
     return <Pieza texto="&#9822;" color={color} />;
 };
 
-Caballo.puedeMoverse = function(celda, fichaActiva) {
+Caballo.puedeMoverse = function(celda, fichaActiva, fichas) {
+    let difColumna = Math.abs(celda.columna - fichaActiva.columna);
+    let difFila = Math.abs(celda.fila - fichaActiva.fila);
+
+    if (
+        (difColumna === 1 && difFila === 2) ||
+        (difColumna === 2 && difFila === 1)
+    ) {
+        let fichaDestino = fichas.find(
+            (ficha) =>
+                ficha.fila === celda.fila && ficha.columna === celda.columna
+        );
+
+        if (fichaDestino && fichaActiva.color === fichaDestino.color) {
+            return false;
+        }
+
+        return true;
+    }
+
     return false;
 };
 
