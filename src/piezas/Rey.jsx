447b93c01ce60be09680cb4b78412df84901984d
marcant94 <mcantelar@gmail.com>
2023-04-18 02:02:21 +0200

    añadimos movimientos del rey

--- src/piezas/Rey.jsx
+++ src/piezas/Rey.jsx
@@ -9,6 +9,30 @@ Rey.calcularCeldasDestino = function(fichaActiva, fichas) {
     // Calculamos todas las celdas de destino posibles
     let celdasDestino = [];
 
+    for (
+        let fila = fichaActiva.fila - 1;
+        fila <= fichaActiva.fila + 1;
+        fila++
+    ) {
+        for (
+            let columna = fichaActiva.columna - 1;
+            columna <= fichaActiva.columna + 1;
+            columna++
+        ) {
+            let fichaDestino = fichas.find(
+                (ficha) => ficha.fila === fila && ficha.columna === columna
+            );
+
+            if (!(fichaDestino && fichaActiva.color === fichaDestino.color)) {
+                // No puede comerse una ficha del mismo color
+                celdasDestino.push({
+                    fila: fila,
+                    columna: columna,
+                });
+            }
+        }
+    }
+
     return celdasDestino;
 };
 
