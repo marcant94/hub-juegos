d72bc8ec3012e7357e4b98953896833560d4be87
Mario Cantelar <mcantelar@gmail.com>
2024-04-21 02:18:21 +0200

    actualizado a node20

--- run-dev-bun.sh
+++
@@ -1,5 +0,0 @@
-
-docker-compose -f ./dockerdev-bun-compose.yml up -d --build
-docker image prune -f
-docker logs --follow juegos_react_dev_bun
-# docker exec -it juegos_react_dev_bun sh
--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,3 @@
 docker-compose -f ./dockerdev-compose.yml up -d --build
 docker image prune -f
 docker logs --follow juegos_react_dev
-# docker exec -it juegos_react_dev sh
