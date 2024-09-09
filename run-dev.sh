4f83ae52cf23c32fe1d6ca7acc1a889f657b8b14
marcant94 <mcantelar@gmail.com>
2024-09-09 19:05:29 +0200

    pasamos a compose v2

--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,4 @@
 
-docker-compose -f ./dockerdev-compose.yml up -d --build
+docker compose -f ./dockerdev-compose.yml up -d --build
 docker image prune -f
 docker logs --follow juegos_react_dev
--- run-prod.sh
+++ run-prod.sh
@@ -1,5 +1,5 @@
 
-docker-compose -f ./docker-compose.yml up -d --build
+docker compose -f ./docker-compose.yml up -d --build
 docker image prune -f
 
 # docker logs --follow juegos_react
