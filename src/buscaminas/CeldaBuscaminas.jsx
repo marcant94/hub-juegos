76684d4e2485e9f8b6c476d2be589203c264f83f
marcant94 <mcantelar@gmail.com>
2024-09-23 21:38:58 +0200

    celda con colores

--- src/buscaminas/CeldaBuscaminas.jsx
+++ src/buscaminas/CeldaBuscaminas.jsx
@@ -4,14 +4,52 @@ import estilos from "./CeldaBuscaminas.module.css";
 
 const CeldaBuscaminas = ({ fila, columna, tieneMina = false, minasAlrededor = 0, descubierto = false, funcionPulsarCelda }) => {
     let textoMostrar = "";
-    if (descubierto && minasAlrededor > 0) {
-        textoMostrar = tieneMina ? "*" : minasAlrededor;
+    let claseColor = "";
+
+    if (descubierto) {
+        if (tieneMina) {
+            textoMostrar = "*";
+        } else {
+            textoMostrar = minasAlrededor;
+
+            switch (minasAlrededor) {
+                case 1:
+                    claseColor = estilos.uno;
+                    break;
+                case 2:
+                    claseColor = estilos.dos;
+                    break;
+                case 3:
+                    claseColor = estilos.tres;
+                    break;
+                case 4:
+                    claseColor = estilos.cuatro;
+                    break;
+                case 5:
+                    claseColor = estilos.cinco;
+                    break;
+                case 6:
+                    claseColor = estilos.seis;
+                    break;
+                case 7:
+                    claseColor = estilos.siete;
+                    break;
+                case 8:
+                    claseColor = estilos.ocho;
+                    break;
+
+                default:
+                    textoMostrar = "";
+                    break;
+            }
+        }
     }
 
     return (
         <div
-            className={estilos.celda + " " + (tieneMina ? estilos.mina : "") + " " + (descubierto ? estilos.descubierto : "")}
+            className={estilos.celda + " " + (tieneMina ? estilos.mina : "") + " " + (descubierto ? estilos.descubierto : "") + " " + claseColor}
             onClick={funcionPulsarCelda.bind(this, fila, columna)}
+            onAuxClick={funcionPulsarCelda.bind(this, fila, columna)}
         >
             {textoMostrar}
         </div>
